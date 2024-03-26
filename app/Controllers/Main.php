b582a7c84a67521b6c2da3c9684caa9f1e633649
Faustino Alfredo <faustinoalfredo06@gmail.com>
2024-03-26 23:39:46 +0100

    Criação de Model & Comunicação com Dados

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -3,12 +3,18 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UsuariosModel;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class Main extends BaseController
 {
     public function index()
     {
-        return view('index');
+        $model = new UsuariosModel(db_connect());
+        $usuarios = $model->findAll();
+        //dd($usuarios);
+
+        echo '<pre>';
+        print_r($usuarios);
     }
 }
